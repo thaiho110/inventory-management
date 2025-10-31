b2f387d57d55c4520c153eeaec33ffd78f21dbbf
Hồ Quốc Thái <thaiho500@gmail.com>
2025-10-31 12:31:36 +0700

    refactor: fix types issue

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import ProductCharts from "@/components/productcharts";
 import Sidebar from "@/components/sidebar";
+import { ProductData } from "@/lib/actions/products";
 import { getCurrentUser } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
 import { DollarSign, TrendingUp } from "lucide-react";
@@ -29,7 +30,7 @@ export default async function DashboardPage() {
         }),
 
     ]);
-    const totalValue = allProducts.reduce((sum: number, product) => sum + Number(product.price) * Number(product.quantity), 0);
+    const totalValue = allProducts.reduce((sum: number, product: ProductData) => sum + Number(product.price) * Number(product.quantity), 0);
 
     const inStockCount = allProducts.filter(product => Number(product.quantity) > 5).length;
     const lowStockCount = allProducts.filter(product => Number(product.quantity) <= 5 && Number(product.quantity) >=1).length;
