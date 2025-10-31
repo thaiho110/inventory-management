69f36d3d153b9983a1409c0dee78efc5a2c2040c
Hồ Quốc Thái <thaiho500@gmail.com>
2025-11-01 05:59:47 +0700

    refactor: update prisma config

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/prisma/generated/prisma/client";
+
 
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
