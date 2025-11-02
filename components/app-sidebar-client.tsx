be6bfcd4e0dbb2a038fe78e803defe475dad41b2
Hồ Quốc Thái <92258137+thaiho110@users.noreply.github.com>
2025-11-02 20:28:05 +0700

    refactor: fixing sidebar loading on restricted page

--- components/app-sidebar-client.tsx
+++ components/app-sidebar-client.tsx
@@ -4,7 +4,7 @@ import { usePathname } from "next/navigation";
 import { AppSidebar } from "./app-sidebar";
 
 export default function AppSidebarClient() {
-  const restrictedPaths = ["/handler/sign-in", "/sign-in", "/"];
+  const restrictedPaths = ["/handler/sign-in", "/handler/sign-up", "/sign-in", "/sign-up"];
   const pathname = usePathname() ?? "/";
 
   if (restrictedPaths.includes(pathname)) {
